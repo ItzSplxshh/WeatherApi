e8b9d57941cedef7015e34c5d4b411f2cf7f9b64
ItzSplxshh <154111370+ItzSplxshh@users.noreply.github.com>
2025-04-18 22:41:53 +0100

    Initial commit: Set up weather API project with controller and external API integration

--- src/main/java/org/splxshh/weatherapi/WeatherController.java
+++ src/main/java/org/splxshh/weatherapi/WeatherController.java
@@ -1,15 +1,33 @@
 package org.splxshh.weatherapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //defines class as a controller and includes @responsebody
 @RequestMapping("weather")
-public class Controller {
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
     @GetMapping("/test")
-    public String test(){
-        return "The Weather API is live!";
+    public ResponseEntity<String> test(@RequestParam String city){
+        ResponseEntity<String> response = weatherService.getWeather(city);
+        if (response.getStatusCode().is2xxSuccessful()){
+            return ResponseEntity.ok(response.getBody());
+        }else {
+            return ResponseEntity
+                    .status(response.getStatusCode())
+                    .body("Something went wrong with the weather API.");
+        }
     }
 
 }
--- src/main/java/org/splxshh/weatherapi/WeatherService.java
+++ src/main/java/org/splxshh/weatherapi/WeatherService.java
@@ -1,14 +1,18 @@
 package org.splxshh.weatherapi;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-public class Service {
+@Service
+public class WeatherService {
     @Value("${weatherApiKey}")
     private String apiKey;
-    public void getWeather(String city){
+    public ResponseEntity<String> getWeather(String city){
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={your_api_key}";
-        restTemplate.getForObject(url, String.class, city, apiKey );
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class, city, apiKey );
+        return response;
     }
 }
